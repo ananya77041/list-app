dfd0b5e5bbefd29486e89c835b1e5895557dc002
Andy Mishra <andy.mishra@opower.com>
2015-07-07 00:05:54 -0400

    improved UI and design

--- js/list.js
+++ js/list.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
+	// $("body").droppable();
+
 	// Adding new item
 	$(this).keydown(function(event) {
-		if (event.which == 13) {
+		if (event.which == 13 && $('input').val() != '') {
 			var newItem = $('input').val();
 			// Create new item with value
-			$(".items").append("<div class='item'><p class='item-name'>" + newItem + "</p><button name='done' class='done'>Done!</button><textarea class='item-desc' placeholder='Enter description here.'></textarea></div>");
+			$(".items").append("<div class='item' style='display: none'><p class='item-name'>" + newItem + "</p><button name='done' class='done'>Done!</button><textarea class='item-desc' placeholder='Enter details here.'></textarea></div>");
+			$(".item").show('fast');
+			$(".items").sortable({
+				containment: '.main',
+				items: '.item',
+				axis: 'y'
+			});
+			$(".item").disableSelection();
+			$('input').val('');
 		};
 	});
 
